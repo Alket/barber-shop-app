59e5cc1000af4be1f4d6e86d462ef7f1d95ad72d
Alket Shyti <alket.shyti@gmail.com>
2025-08-15 12:46:50 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,7 @@ html {
   --font-mono: ${GeistMono.variable};
 }
         `}</style>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
       </head>
       <body>
         <AuthProvider>
